efc456c78c79c970afe9cf964a23e360a55dfdc5
Tihomir Garmenliev <101339875+tgarmenliev@users.noreply.github.com>
2026-03-31 20:30:26 +0300

    [FIX] live route

--- routes/live.js
+++ routes/live.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const liveController = require('../controllers/liveController');
 
 // GET /api/live/:language/:stationNumber/:type
-router.get('/:language/:stationNumber/:type', liveController.getLive);
+router.get('/:language/:stationNumber/:type', liveController.getLiveBoard);
 
 module.exports = router;
